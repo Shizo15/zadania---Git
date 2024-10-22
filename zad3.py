b25c4865941526dbe158f579fe38fe420f380709
Shizo <damianszymczyk153@gmail.com>
2024-10-22 15:19:17 +0200

    feat: added task 3

--- zad3.py
+++ zad3.py
@@ -18,4 +18,5 @@ print("Sprawdzianie progu krytyczności")
 temperature = float(input("Podaj temperaturę w Kelwinach: "))
 neutrons_per_second = float(input("Podaj przepływ neutronów na sekundę: "))
 threshold = float(input("Podaj wartość progową: "))
+
 print(fail_safe(temperature, neutrons_per_second, threshold))
